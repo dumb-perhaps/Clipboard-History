c8bd99c9ff7e1aa6edc2a0daca4d11127c3ce3fd
dumb-perhaps <limpidmelange@gmail.com>
2026-01-21 01:14:24 -0800

    removed all functions from main_.py
    removed all functions from main_.py and made a new function module

--- main_.py
+++ main_.py
@@ -1,12 +1,15 @@
 import dearpygui.dearpygui as dpg
 import pyperclip
+from func import set_accent
+from func import open_settings_menu
+from func import btn_callback
 
 history = []
 max_history = 50
 dpg.create_context()
 
 with dpg.font_registry():
-    with dpg.font("CaskaydiaCoveNerdFontMono-SemiBold.ttf", 16) as default_font:
+    with dpg.font("/home/nigger/Downloads/Fonts/CascadiaCode/CaskaydiaCoveNerdFontMono-SemiBold.ttf", 16) as default_font:
         dpg.add_font_range_hint(dpg.mvFontRangeHint_Default)
         dpg.add_font_range(0x0020, 0x00FF)  # Basic Latin + Latin Supplement
         dpg.add_font_range(0x2000, 0x206F)  # General Punctuation
@@ -54,33 +57,7 @@ with dpg.theme(tag="settings_window_theme"):
         dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 4)
 
 
-def set_accent(sender):
-    color_map = {
-        1025: (220, 50, 50, 255),
-        1026: (50, 220, 100, 255),
-        1027: (50, 100, 220, 255),
-        1028: (240, 200, 50, 255),
-        1029: (180, 100, 200, 255)
-    }
 
-    for cb in [1025, 1026, 1027, 1028, 1029]:
-        if cb != sender:
-            dpg.set_value(cb, False)
-
-    dpg.set_value("bg_color", color_map[sender])
-
-
-def open_settings_menu(sender, app_data, user_data):
-    if dpg.is_item_shown("settings_menu"):
-        dpg.hide_item("settings_menu")
-    else:
-        dpg.show_item("settings_menu")
-
-
-def btn_callback(sender):
-    config = dpg.get_item_configuration(sender)
-    btn_label = config['label']
-    pyperclip.copy(btn_label)
 
 
 with dpg.window(label="Clipboard Manager", no_title_bar=True, tag="id", width=800, height=400):
